567401ee5f0440d3327bcdb9d9f9b67f01bdf287
Jacob Alberty <jacob.alberty@gmail.com>
2020-10-19 16:36:04 -0500

    trytravis-2020-10-19T16:36:04.206687

--- ci.sh
+++ ci.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  docker buildx build \
-    --progress plain \
-    --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-    .
+getTags() {
+  BRANCH="${TRAVIS_BRANCH:-latest}"
+  if [ $BRANCH = 'master' ]; then
+    BRANCH=latest
+  fi
+  echo --tag $DOCKER_REPO:$BRANCH
+  if [ -f tags.list ]; then
+    for tag in $(cat tags.list); do
+      echo --tag $DOCKER_REPO:$tag
+    done
+  fi
+}
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#  docker buildx build \
+#    --progress plain \
+#    --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+#    .
 
-  docker build -t unifi:latest .
+#  docker build -t unifi:latest .
 
-  docker run -d -p 8443:8443 -p 8080:8080 -e PKGURL --name unifi unifi:latest
-  docker ps | grep -q unifi
-  docker logs unifi
-  sleep 10 && curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 -kILs --fail http://127.0.0.1:8080 || exit 1
-  exit $?
-fi
-BRANCH="${TRAVIS_BRANCH:-latest}"
-if [ $BRANCH = 'master' ]; then
-  BRANCH=latest
-fi
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /dev/null
-docker buildx build \
+#  docker run -d -p 8443:8443 -p 8080:8080 -e PKGURL --name unifi unifi:latest
+#  docker ps | grep -q unifi
+#  docker logs unifi
+#  sleep 10 && curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 -kILs --fail http://127.0.0.1:8080 || exit 1
+#  exit $?
+#fi
+
+#echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /dev/null
+echo docker buildx build \
   --progress plain \
   --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  -t $DOCKER_REPO:$BRANCH \
+  $(getTags) \
   --push \
   .
